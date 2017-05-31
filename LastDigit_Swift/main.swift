d1afdb87ce27dbc2e7ecd280a4480e70dd5b6738
leonguevara <leon.guevara@outlook.com>
2017-05-31 10:50:24 -0500

    Initial Commit

---
+++ LastDigit_Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LastDigit_Swift
+//
+//  Created by León Felipe Guevara Chávez on 5/31/17.
+//  Copyright © 2017 León Felipe Guevara Chávez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
